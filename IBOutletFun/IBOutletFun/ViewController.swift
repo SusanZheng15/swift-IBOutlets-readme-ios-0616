b50b71dd783ff3dba1c726c6e1c0a34eb3da570a
SusanZheng15 <zheng.susan12@gmail.com>
2016-07-20 10:45:37 -0400

    Done

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Zzz"
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func helloWorld(sender: AnyObject)
+    {
+        print("Print hello world")
+        label.text = "Hello World"
+    }
+    @IBAction func goodByeWorld(sender: AnyObject)
+    {
+        print("Print Goodbte World")
+        label.text = "Goodbye World"
+    }
+    @IBAction func unicorn(sender: AnyObject)
+    {
+        print("Print Unicorn")
+        label.text = "Unicorn!"
     }
 }
